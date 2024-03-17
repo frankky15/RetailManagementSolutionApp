0d903ad779941915528e320fd625218f890aaaeb
Franco Castro <francocramiro@gmail.com>
2024-03-17 16:47:17 -0300

    Initial Commit

---
+++ RMS/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RMS.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
